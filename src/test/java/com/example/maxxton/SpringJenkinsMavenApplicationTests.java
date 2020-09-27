6588220e45a772063dd2dbdddfdbfce669bf30e0
s.sonje@maxxton.com <s.sonje@maxxton.com>
2020-09-27 11:44:33 +0530

    unit test

--- src/test/java/com/example/maxxton/SpringJenkinsMavenApplicationTests.java
+++ src/test/java/com/example/maxxton/SpringJenkinsMavenApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.maxxton;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SpringJenkinsMavenApplicationTests {
 
+  @Mock
+  private HelloWorldController helloWorldController;
+
 	@Test
 	void contextLoads() {
 	}
+
+  @Test
+  public void testHelloWorld() {
+    String result = helloWorldController.getHelloWorld();
+    assertEquals("Hello World!", result);
+  }
 
 }
